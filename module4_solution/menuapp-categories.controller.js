4a72a72873baab25d3d003ef415ac1c28e5f7ed4
Mayar Salah ELDin <mayar_salah13@yahoo.com>
2020-05-14 23:10:00 +0200

    last

---
+++ module4_solution/menuapp-categories.controller.js
@@ -0,0 +1,20 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('data')
+    .controller('CategoriesCtrl', CategoriesCtrl);
+
+  CategoriesCtrl.$inject = ['categoriesList']
+  function CategoriesCtrl(categoriesList) {
+
+    // "categories" è la label del controller definito nello state
+    var categories = this;
+    categories.pageTitle = 'Categorie';
+
+    // categoriesList contiene la promise definita nel resolve dello state
+    // per richiamare la lista di categorie dal servizio menudata.service.js
+    categories.categoriesList = categoriesList;
+  }
+
+}());
---
+++ module4_solution/menudata.service.js
@@ -0,0 +1,45 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('data')
+    .service('MenuDataService', MenuDataService);
+
+
+  MenuDataService.$inject = ['$http'];
+  function MenuDataService ($http) {
+    var mds = this;
+
+    // getAllCategories :: this method should return a promise
+    // which is a result of using the $http service,
+    // REST API endpoint: https://davids-restaurant.herokuapp.com/categories.json
+    mds.getAllCategories = function () {
+      return $http({
+        method: 'GET',
+        url: 'https://davids-restaurant.herokuapp.com/categories.json'
+      }).then(function(result){
+        return result.data;
+      })
+    };
+
+    // getItemsForCategory(categoryShortName)
+    // this method should return a promise which is a result of using the $http service
+    // REST API endpoint: https://davids-restaurant.herokuapp.com/menu_items.json?category=,
+    // before the call to the server, your code should append whatever categoryShortName value was passed in as an argument into the getItemsForCategory method.
+    mds.getItemsForCategory = function(categoryShortName) {
+
+      return $http({
+        method: 'GET',
+        url: 'https://davids-restaurant.herokuapp.com/menu_items.json',
+        params: {
+          category: categoryShortName
+        }
+      }).then(function(result){
+        return result.data.menu_items;
+      })
+
+    }
+
+  }
+
+}());
